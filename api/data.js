15411b863b04ecea541c75c8adbfd3e90ae79818
m4d4r4uchih4 <gb358159@gmail.com>
2025-04-14 17:24:41 +0600

    Delete api directory

--- api/data.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  const data = [
-    { id: 1, name: "Item One", value: "Value 1" },
-    { id: 2, name: "Item Two", value: "Value 2" },
-  ];
-  res.status(200).json(data);
-}
